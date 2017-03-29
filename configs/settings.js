9fbd2193002b7211a7923663dfea474943b2c127
maoq <maoqxm@appest.com>
2017-03-29 08:37:29 +0800

    兑换码邮件更新

--- configs/settings.js
+++ configs/settings.js
@@ -3,11 +3,12 @@ module.exports = {
   package_mode: false,
   product_name: '滴答清单',
   cdn_domain: 'cdn-dida365-com.alikunlun.com',
-  site_domain: 'dida365.com',
+  site_domain: 'ticktick.com',
   brother_product_name: 'TickTick',
   brother_site_domain: 'ticktick.com',
   api_domain: 'api.dida365.com',
   protocol: 'http://',
   help_domain: 'help.dida365.com',
-  deploy_path: '/task/src/main/webapp/static/deploy'
+  deploy_path: '/task/src/main/webapp/static/deploy',
+  $codes: [{id:1}, {id:2}]
 }
